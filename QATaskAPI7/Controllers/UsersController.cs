792829183e1a8d3ef80a57dbc4c935ab2fe2e849
Nikites <rockstar.gg@mail.ru>
2023-05-03 22:38:07 +0300

    add delete put api

--- QATaskAPI7/Controllers/UsersController.cs
+++ QATaskAPI7/Controllers/UsersController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace QATaskAPI7.Controllers
 {
     public class UsersController : ApiController
     {
         [HttpGet]
-        [Route("api/user/{id}")]
+        [Route("api/user/get/{id}")]
         public IHttpActionResult GetUser(int id)
         {
             User user = new User();
@@ -26,14 +28,59 @@ namespace QATaskAPI7.Controllers
 
             return Ok(new { user });
         }
+        
+        [HttpGet]
+        [Route("api/users/get/")]
+        public IHttpActionResult GetUsers()
+        {
+            List<User> list = new List<User>();
+            User user = new User();
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            user.Id = 2;
+            user.Name = "TestName";
+            user.Email = "test@test.test";
+            
+            list.Add(user);
+            list.Add(user);
+            list.Add(user);
+            list.Add(user);
+
+            return Ok(new { list });
+        }
 
         [HttpPost]
-        [Route("api/user/")]
-        public IHttpActionResult PosUser(User user)
+        [Route("api/user/post/")]
+        public IHttpActionResult Post(int id, String name, String email)
         {
             User newUser = new User();
-            newUser = user;
+            newUser.Id = id;
+            newUser.Email = email;
+            newUser.Name = name;
 
+            return Ok(newUser);
+        }
+
+        [HttpDelete]
+        [Route("api/user/delete/{name}")]
+        public IHttpActionResult DeleteUser(String name)
+        {
+          
+           return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/user/put/{id}")]
+        public async Task<IHttpActionResult>  PutUser(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return BadRequest();
+            }
             return Ok();
         }
     }
